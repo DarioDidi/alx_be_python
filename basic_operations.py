e03fb0351b834e78aee2d8f9cb8f7b8e2e770898
DD-tech <>
2024-09-09 08:54:49 +0300

    added basic operations file

---
+++ basic_operations.py
@@ -0,0 +1,23 @@
+'''
+    Create a file named basic_operations.py.
+    In this file, you will define two variables: number1 and number2, with the values 10 and 5, respectively.
+    You do not need to write any functions or import any modules.
+    Calculate the sum, difference (by subtracting number2 from number1), and product of number1 and number2.
+    Print the results of each operation in the format: [operation] of [number1] and [number2] is [result].
+
+    Addition of 10 and 5 is 15
+Subtraction of 10 and 5 is 5
+Multiplication of 10 and 5 is 50
+Expected Output:
+Perform addition, subtraction, and multiplication on these numbers.
+When executed, your script should print the following (assuming number1 = 10 and number2 = 5)'''
+
+number1 = 10
+number2 = 5
+
+print(f"Addition of {number1} and {number2} is {number1 + number2}")
+print(f"Subtraction of {number1} and {number2} is {number1 - number2}")                 
+print(f"Multiplication of {number1} and {number2} is {number1 * number2}")
+#print(f"Division of {number1} and {number2} is {number1 / number2}")
+
+
