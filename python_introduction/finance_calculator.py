1bcfee6621c65ec98039019adac840c25dc44ca9
DD-tech <>
2024-09-09 10:10:15 +0300

    had wrong file

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -5,4 +5,4 @@ monthly_savings = int(monthly_income) - int(monthly_expenses)
 projected_savings = monthly_savings * 12 * 1.05
 
 print(f"Your monthly savings are {monthly_savings}.")
-print(f"Projected savings after one year, with interest, is: ${projected_savings}")
+print(f"Projected savings after one year, with interest, is: ${projected_savings}.")
