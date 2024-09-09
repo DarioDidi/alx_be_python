9ad878f6c70a00b74ad7138c2924382708dc94eb
DD-tech <>
2024-09-09 09:47:16 +0300

    relocated finance calculator

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,8 @@
+monthly_income = input("Enter your monthly income: ")
+monthly_expenses =  input("Enter your total monthly expenses: ")
+monthly_savings = int(monthly_income) - int(monthly_expenses)
+
+projected_savings = monthly_savings * 12 * 1.05
+
+print(f"Your monthly savings are {monthly_savings}.")
+print(f"Projected savings after one year, with interest, is: ${projected_savings}")
