0f9528c88d39a0ded544b97d404815cbb6218a9c
DD-tech <>
2024-09-15 15:16:08 +0300

    removed lines

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,6 @@
 monthly_income = input("Enter your monthly income: ")
 monthly_expenses =  input("Enter your total monthly expenses: ")
-#monthly_savings = int(monthly_income) - int(monthly_expenses)
-
 monthly_savings = int( monthly_income) - int(monthly_expenses)
 savings = int(monthly_savings * 12 * 1.05)
-
 print(f"Your monthly savings are ${monthly_savings}.")
 print(f"Projected savings after one year, with interest, is: ${savings}.")
