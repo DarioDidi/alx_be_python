fea745eb20f276e52a2750ca1f1a97372dd44f16
DD-tech <>
2024-09-15 19:49:13 +0300

    addes new lines to input

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,5 +1,5 @@
-monthly_income = input("Enter your monthly income: ")
-monthly_expenses =  input("Enter your total monthly expenses: ")
+monthly_income = input("Enter your monthly income: \n")
+monthly_expenses =  input("Enter your total monthly expenses: \n")
 monthly_savings = int(monthly_income) - int(monthly_expenses)
 annual_savings = int(monthly_savings * 12 * 1.05)
 print(f"Your monthly savings are ${str(monthly_savings)}.")
