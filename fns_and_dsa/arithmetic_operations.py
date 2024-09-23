9425723fc5a2da086522c8f9dc187e737374143a
DD-tech <>
2024-09-23 09:38:22 +0300

    added elif for tests

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,7 +9,7 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 result = "Cannot divide by zero."
-            else: result = num1 / num2
+            elif num2 != 0: result = num1 / num2
         case _:
             result = "Operation not recognized"
     return result
