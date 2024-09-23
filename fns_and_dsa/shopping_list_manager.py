cd16d79271f6ab32cb0c0e8a09267177b2fc5e62
DD-tech <>
2024-09-23 09:52:10 +0300

    edited input message in shopping list manager added the

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -5,6 +5,7 @@ def display_menu():
     print("3. View List")
     print("4. Exit")
 
+
 def main():
     shopping_list = []
     while True:
@@ -12,21 +13,23 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            to_add = input("Enter item to add: ")
+            # to_add = input("Enter item to add: ")
+            to_add = input("Enter the item to add: ")
             shopping_list.append(to_add)
         elif choice == '2':
-            to_remove = input("Enter item to remove: ")
+            to_remove = input("Enter the item to remove: ")
             if to_remove in shopping_list:
                 shopping_list.remove(to_remove)
             else:
-                print("Item not found")            
+                print("Item not found")
         elif choice == '3':
-            print(shopping_list)            
+            print(shopping_list)
         elif choice == '4':
             print("Goodbye!")
             break
         else:
             print("Invalid choice. Please try again.")
+
 
 if __name__ == "__main__":
     main()
