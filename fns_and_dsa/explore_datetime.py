2ae5379021872505eb21248b17bb0d2ea135f207
DD-tech <>
2024-09-23 10:13:29 +0300

    added explore datettime file

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current date and time: {current_date}")
+
+def calculate_future_date():
+    num_days = int(input("Enter the number of days to add to the current date: "))
+    future_date = datetime.now() + timedelta(days=num_days)
+    future_date = future_date.strftime("%Y-%m-%d")
+    print(f"Future date: {future_date}")
+    
+if __name__ == "__main__":
+    display_current_datetime()
+    calculate_future_date()
