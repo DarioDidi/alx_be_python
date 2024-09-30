4b41cb940ecd1a3332d976f6a412e5192b111c9a
DD-tech <>
2024-09-30 11:47:14 +0300

    changed private to protected??

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -2,39 +2,39 @@ class Book:
     def __init__(self, title=None, author=None):
         self.title = title
         self.author = author
-        self. __is_checked_out = False
+        self. _is_checked_out= False
 
     def check_out(self):
-        self.__is_checked_out = True
+        self._is_checked_out= True
 
     def return_book(self):
-        self.__is_checked_out = False
+        self._is_checked_out= False
 
     def available(self):
-        return not self.__is_checked_out
+        return not self._is_checked_out
 
 
 class Library:
     def __init__(self):
-        self.__books = []
+        self._books = []
 
     def add_book(self, book):
         # book = Book(title=title)
-        self.__books.append(book)
+        self._books.append(book)
 
     def check_out_book(self, title):
-        for book in self.__books:
+        for book in self._books:
             if book.title == title:
                 book.check_out()
                 break
 
     def return_book(self, title):
-        for book in self.__books:
+        for book in self._books:
             if book.title == title:
                 book.return_book()
                 break
 
     def list_available_books(self):
-        for book in self.__books:
+        for book in self._books:
             if book.available():
                 print(f"{book.title} by {book.author}")
