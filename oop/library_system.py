0849c3728bcc6b66a8441a6c92fd9c31a3bff2e8
DD-tech <>
2024-10-07 16:28:32 +0300

    corrected typos

--- oop/library_system.py
+++ oop/library_system.py
@@ -13,7 +13,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"{type(self).__name__}: {self.title} by {self.author}, File Sie: {self.file_size}"
+        return f"{type(self).__name__}: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
 class PrintBook(Book):
